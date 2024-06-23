b80ae7f40ab50b9eb81042538e016bdd1f0ac26c
Nagakumar2402 <Nagakumar2402@gmail.com>
2024-06-24 00:13:01 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+import "./src/style/modern-normalize.css";
+import "./src/style/style.css";
+import "./src/style/components/header.css";
+import "./src/style/components/hero.css";
+import "./src/style/components/feature.css";
+import "./src/style/utils.css";
