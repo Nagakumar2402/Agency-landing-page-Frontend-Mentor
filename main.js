5e0724e7cd3f37be592d840edc0bd3cc521575c3
nagakumar2402 <nagakumar2402@gmail.com>
2024-06-24 18:11:56 +0530

    testimonials

--- main.js
+++ main.js
@@ -3,4 +3,5 @@ import "./src/style/style.css";
 import "./src/style/components/header.css";
 import "./src/style/components/hero.css";
 import "./src/style/components/feature.css";
+import "./src/style/components/testimonials.css";
 import "./src/style/utils.css";
