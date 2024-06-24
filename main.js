7cff85fb2afcf9537c140cd9c1c03ca64d845d1e
Nagakumar2402 <Nagakumar2402@gmail.com>
2024-06-24 23:36:04 +0530

    mobile menu

--- main.js
+++ main.js
@@ -4,4 +4,7 @@ import "./src/style/components/header.css";
 import "./src/style/components/hero.css";
 import "./src/style/components/feature.css";
 import "./src/style/components/testimonials.css";
+import "./src/style/components/gallery.css";
+import "./src/style/components/footer.css";
+import "./src/style/components/mobile-nav.css";
 import "./src/style/utils.css";
---
+++ src/style/components/scroll.js
@@ -0,0 +1,4 @@
+//copy node list
+const copy = document.querySelector(".image-gallery").cloneNode(true);
+//append
+document.querySelector(".gallery").appendChild(copy);
